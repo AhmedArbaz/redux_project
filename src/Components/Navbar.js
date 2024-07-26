ec378c086fea49299f2c6d209fba5ce0bb9005df
AhmedArbaz <ah2797764@gmail.com>
2024-07-26 11:33:00 +0500

    using our reducer in Navbar and solve a mistake in amountReducer

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,29 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+    const amount = useSelector(state=>state.amount)
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">State Bank of Ahmed</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">State Bank of Ahmed</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/about">About</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/about">About</a>
                             </li>
                           
                         </ul>
                         
                         <div>
-                            <button disabled={true} className="btn btn-primary ">Your Balance is 0</button>
+                            <button disabled={true} className="btn btn-primary ">Balance: {amount}</button>
                         </div>
                     </div>
                 </div>
--- src/state/reducer/amountReducer.js
+++ src/state/reducer/amountReducer.js
@@ -1,6 +1,6 @@
 // ham action-craetor banty hain jo kay hamin batata hay kay reducer kia karay aur reducer bhot saray ho sakty hin e.g deposit, withdraw,ager normal example dakhin to bacha hamara ui tha aur reducer ko bhot kam ho sakty hain bachy ko khana khilana , sleep ,etc reducer ko insturction 
 
-const reducer = (state, action) => {
+const reducer = (state=0, action) => {
     if(action.type === 'deoposit'){
         return state + action.payload
     }
